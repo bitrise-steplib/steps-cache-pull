10e5156284076ee226062a09b3fef4cd9f8a45a3
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-10-22 09:25:37 +0200

    Revert "revert extract fix"
    This reverts commit 10775056e848faf03d90ae0c37e8012686108771.

--- archive.go
+++ archive.go
@@ -27,8 +27,7 @@ func uncompressArchive(pth string) error {
 
 // extractCacheArchive invokes tar tool by piping the archive to the command's input.
 func extractCacheArchive(r io.Reader) error {
-	// cmd := command.New("tar", "-xPf", "-")
-	cmd := command.New("tar", "-xPf", "/dev/stdin")
+	cmd := command.New("tar", "-xPf", "-")
 	cmd.SetStdin(r)
 	if out, err := cmd.RunAndReturnTrimmedCombinedOutput(); err != nil {
 		errMsg := err.Error()
